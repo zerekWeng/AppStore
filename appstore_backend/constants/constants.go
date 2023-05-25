a66bd40dbfeea8ba0aebd38e821e60f18472ef50
zerekWeng <zerekweng@gmail.com>
2023-05-24 20:11:02 -0700

    Initial Version

---
+++ appstore_backend/constants/constants.go
@@ -0,0 +1,11 @@
+package constants
+
+const (
+    APP_INDEX = "app"
+    USER_INDEX = "user"
+    ES_URL = "http://10.138.0.2:9200"
+    ES_USERNAME = "zerekweng"
+    ES_PASSWORD = "appstore"
+    STRIPE_API_KEY = "sk_test_51N4u0lLpFEOiMu9kqhfU38KYCpkx61ymh1cbUVsWQ1gOpZD1UJo6CExwJAV0XGWYviwCRj3cBwtS5Cod2WA9dU9W00UfCusxPz"
+    GCS_BUCKET = "appstore_bucket"
+)
